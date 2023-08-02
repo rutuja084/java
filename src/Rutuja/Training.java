8eb397913e9f7eba3b43122a4384735a3d042076
HP <HP@LAPTOP-RM65GEDV.mshome.net>
2023-08-02 19:55:13 +0530

    update

---
+++ src/Rutuja/Training.java
@@ -0,0 +1,19 @@
+package Rutuja;
+
+public class Training {
+
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int i=15;
+			int j=20;
+			
+			System.out.println(i);
+			System.out.println(j);
+		}
+
+	
+
+	}
+
+
